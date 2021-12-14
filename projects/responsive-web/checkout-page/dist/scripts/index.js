6220bfea0d1e87494ff72c633e00b3ce1c6d3f7a
TarekVisch <tarek.chentouf.99@gmail.com>
2021-12-14 20:40:43 +0100

    Add html mockup for select field

--- projects/responsive-web/checkout-page/dist/scripts/index.js
+++ projects/responsive-web/checkout-page/dist/scripts/index.js
@@ -1,1 +1,21 @@
-console.log('Hello World !');
+// const EMAIL_REGEX =
+//   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// const PHONE_REGEX = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g;
+
+// const FROM_CRITERIA = {
+//   email: { required: true, regex: EMAIL_REGEX },
+//   phone: { required: true, regex: PHONE_REGEX },
+//   fullName: { required: true },
+//   address: { required: true }
+// };
+
+// const form = document.getElementById('form');
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   const formInputs = Array.from(form.elements).filter(
+//     (el) => el.tagName !== 'BUTTON'
+//   );
+
+//   console.log(formInputs[5]);
+// });
